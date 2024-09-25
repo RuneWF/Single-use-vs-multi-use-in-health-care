aec7e4cdf505f43e7bccfcd16d6e1f72585e644f
RuneWF <ruw@igt.sdu.dk>
2024-09-25 11:47:08 +0200

    modified:   Brighway/Monte_Carlo.py

--- Brighway/Monte_Carlo.py
+++ Brighway/Monte_Carlo.py
@@ -103,78 +103,79 @@ def MonteCarlo(iterations, FU, impact_category, uncertainties):
     return lcia_results_array
 
 def MC_results(iterations, FU_new, impact_category, uncertainties):
-    MC_idx = ['Mean', 'Median', 'Standard Deviation', 'Minimum', 'Maximum']
+    print('nice')
+#     MC_idx = ['Mean', 'Median', 'Standard Deviation', 'Minimum', 'Maximum']
 
-    if type(impact_category) == tuple:
-        Monte_Carlo_dct = {}
+#     if type(impact_category) == tuple:
+#         Monte_Carlo_dct = {}
         
 
-        # Define the LCIA method
-        # for method in impact_category:
-        print(f'Doing Monte Carlo simulations for {impact_category}')
+#         # Define the LCIA method
+#         # for method in impact_category:
+#         print(f'Doing Monte Carlo simulations for {impact_category}')
 
-        # Create a DataFrame to store results
-        df_MC = pd.DataFrame(0, index=MC_idx, columns=FU_new.keys(), dtype=object)  # dtype=object to handle lists
-        raw_data = []
-        for col in df_MC.columns:
-            print(f'Processing {col}')
-            lcia_results_array = MonteCarlo(iterations, FU_new[col], impact_category, uncertainties[col])
-            raw_data.append(lcia_results_array)
-            for index, row in df_MC.iterrows():
-                # print(index, row[col])
-                if 'mean' in index.lower():
-                    row[col] = np.mean(lcia_results_array)
-                    # print(index)
-                elif 'median' in index.lower():
-                    row[col] = np.median(lcia_results_array)
-                    # print(index)
-                elif 'standard' in index.lower():
-                    row[col] = np.std(lcia_results_array)
-                    # print(index)
-                elif 'minimum' in index.lower():
-                    row[col] = np.min(lcia_results_array)
-                    # print(index)
-                elif 'maximum' in index.lower():
-                    row[col] = np.max(lcia_results_array)
-        return df_MC, raw_data
+#         # Create a DataFrame to store results
+#         df_MC = pd.DataFrame(0, index=MC_idx, columns=FU_new.keys(), dtype=object)  # dtype=object to handle lists
+#         raw_data = []
+#         for col in df_MC.columns:
+#             print(f'Processing {col}')
+#             lcia_results_array = MonteCarlo(iterations, FU_new[col], impact_category, uncertainties[col])
+#             raw_data.append(lcia_results_array)
+#             for index, row in df_MC.iterrows():
+#                 # print(index, row[col])
+#                 if 'mean' in index.lower():
+#                     row[col] = np.mean(lcia_results_array)
+#                     # print(index)
+#                 elif 'median' in index.lower():
+#                     row[col] = np.median(lcia_results_array)
+#                     # print(index)
+#                 elif 'standard' in index.lower():
+#                     row[col] = np.std(lcia_results_array)
+#                     # print(index)
+#                 elif 'minimum' in index.lower():
+#                     row[col] = np.min(lcia_results_array)
+#                     # print(index)
+#                 elif 'maximum' in index.lower():
+#                     row[col] = np.max(lcia_results_array)
+#         return df_MC, raw_data
         
-    else:
-        Monte_Carlo_dct = {}
-        MC_idx = ['Mean', 'Median', 'Standard Deviation', 'Minimum', 'Maximum']
+#     else:
+#         Monte_Carlo_dct = {}
+#         MC_idx = ['Mean', 'Median', 'Standard Deviation', 'Minimum', 'Maximum']
 
-        # Define the LCIA method
-        method = impact_category[1]
-        for method in impact_category:
-            print(f'Doing Monte Carlo simulations for {method[1]}')
+#         # Define the LCIA method
+#         method = impact_category[1]
+#         for method in impact_category:
+#             print(f'Doing Monte Carlo simulations for {method[1]}')
 
-            # Create a DataFrame to store results
-            df_MC = pd.DataFrame(0, index=MC_idx, columns=FU_new.keys(), dtype=object)  # dtype=object to handle lists
-            raw_data = []
-            for col in df_MC.columns:
-                print(f'Processing {col}')
-                lcia_results_array = MonteCarlo(iterations, FU_new[col], method, uncertainties[col])
-                raw_data.append(lcia_results_array)
-                for index, row in df_MC.iterrows():
-                    # print(index, row[col])
-                    if 'mean' in index.lower():
-                        row[col] = np.mean(lcia_results_array)
-                        # print(index)
-                    elif 'median' in index.lower():
-                        row[col] = np.median(lcia_results_array)
-                        # print(index)
-                    elif 'standard' in index.lower():
-                        row[col] = np.std(lcia_results_array)
-                        # print(index)
-                    elif 'minimum' in index.lower():
-                        row[col] = np.min(lcia_results_array)
-                        # print(index)
-                    elif 'maximum' in index.lower():
-                        row[col] = np.max(lcia_results_array)
-                        # print(index)
+#             # Create a DataFrame to store results
+#             df_MC = pd.DataFrame(0, index=MC_idx, columns=FU_new.keys(), dtype=object)  # dtype=object to handle lists
+#             raw_data = []
+#             for col in df_MC.columns:
+#                 print(f'Processing {col}')
+#                 lcia_results_array = MonteCarlo(iterations, FU_new[col], method, uncertainties[col])
+#                 raw_data.append(lcia_results_array)
+#                 for index, row in df_MC.iterrows():
+#                     # print(index, row[col])
+#                     if 'mean' in index.lower():
+#                         row[col] = np.mean(lcia_results_array)
+#                         # print(index)
+#                     elif 'median' in index.lower():
+#                         row[col] = np.median(lcia_results_array)
+#                         # print(index)
+#                     elif 'standard' in index.lower():
+#                         row[col] = np.std(lcia_results_array)
+#                         # print(index)
+#                     elif 'minimum' in index.lower():
+#                         row[col] = np.min(lcia_results_array)
+#                         # print(index)
+#                     elif 'maximum' in index.lower():
+#                         row[col] = np.max(lcia_results_array)
+#                         # print(index)
                     
                     
                         
-            Monte_Carlo_dct[method[1]] = df_MC
-        return Monte_Carlo_dct, raw_data
+#             Monte_Carlo_dct[method[1]] = df_MC
+#         return Monte_Carlo_dct, raw_data
 
                 
